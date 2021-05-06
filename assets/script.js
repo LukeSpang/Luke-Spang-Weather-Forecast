ba2a5c0438260a8e576a59ffc0b6264ec7ea0ada
Luke Spang <lspang20@gmail.com>
2021-05-06 15:09:35 -0400

    html added five day, api requet info for same day and five day added

--- assets/script.js
+++ assets/script.js
@@ -6,18 +6,37 @@ var units = '&units=imperial';
 var input = document.getElementById('location');
 //var input = document.querySelector('#location').val();
 
+var city = document.getElementById('currentCity')
+var temperature = document.getElementById('currentTemp')
+var wind = document.getElementById('currentWind')
+var humidity = document.getElementById('currentHumidity')
+
+var forecastApi = 'http://api.openweathermap.org/data/2.5/forecast?q='
+
+
 
 $('#searchBtn').on('click',function(){
     fetch(api+input.value+apiKey+units)
-    .then(function (response){
-        console.log(response.json());
+    .then(response=>response.json())
+    .then(data=>{
+        var currentCity = data['name']
+        var currentTemp = data['main']['temp']
+        var currentWind = data['wind']['speed']
+        var currentHum = data['main']['humidity']
+    
+        city.innerHTML = `${currentCity}`
+        temperature.innerHTML = `${currentTemp}`
+        wind.innerHTML = `${currentWind}`
+        humidity.innerHTML = `${currentHum}`
     })
+    .catch(console.error);
 })
 
 
-
-
-
-
-
-//http://api.openweathermap.org/data/2.5/weather?q=Raleigh&appid=2d7b3d6c8f91c546eebe47453238d33a&units=imperial
+$('#searchBtn').on('click',function(){
+    fetch(forecastApi+input.value+apiKey+units)
+    .then(response=>response.json())
+    .then(data=>{
+        
+    })
+})
